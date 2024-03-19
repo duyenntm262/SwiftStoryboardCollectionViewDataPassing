24af8d49b5e0d2ae671c7aa95741e9030a516e43
DuyenNTM <dyndyn2602@gmail.com>
2024-03-19 13:39:44 +0700

    Initial Commit

---
+++ CollectionView with DataPassing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView with DataPassing
+//
+//  Created by Din Din on 19/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
